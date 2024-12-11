83e9cbc4969420efb7e7d14bd33003956fae6c3b
Krzysztof Gonia <krzysztof.gonia@cdq.com>
2024-12-11 10:50:24 +0100

    first commit

---
+++ src/main/java/com/example/nativedemo/NativeDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.nativedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(NativeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nativedemo/NativeDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.nativedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
